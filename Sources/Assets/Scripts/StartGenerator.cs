2af9990da98e293b73f575ee3054d3ae0dd78ff0
SuperValou <astry@hotmail.fr>
2014-04-10 14:54:12 +0200

    versionnement des Assets telecharges et fichiers textures

---
+++ Sources/Assets/Scripts/StartGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGenerator : MonoBehaviour {
+	public GameObject generator ;
+	public AudioClip clickButton ;
+	private WiiMote wii = null;
+	
+	void Start (){
+	}
+	
+	void FixedUpdate (){
+	}
+	
+	void OnTriggerStay (Collider col){
+		if (col.gameObject.tag == "Player") {
+			if ((Input.GetKeyDown ("e")) || (wii != null && wii.b_C)) {
+				audio.PlayOneShot(clickButton);
+			}
+		}
+	}
+	
+	void OnTriggerExit (Collider other){
+	}
+}
