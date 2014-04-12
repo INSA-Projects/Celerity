e2dc38b2d99f5d42384b96627eb33528c830d55a
RmLtA <rm.liantsoa@gmail.com>
2014-04-12 23:32:50 +0200

    ajout menu principal

---
+++ Sources/Assets/Scripts/TextControl.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextCsharp : MonoBehaviour {
+	void OnMouseEnter() {
+		renderer.material.color = Color.red;
+	}
+}
+
---
+++ Sources/Assets/Scripts/TextControl.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+var isQuitButton = false;
+var isPlayButton = false;
+var isInstructionButton = false;
+
+
+function OnMouseEnter(){
+	renderer.material.color = Color.green;
+}
+
+function OnMouseExit(){
+	renderer.material.color = Color.grey;
+}
+
+function OnMouseUp(){
+	if(isQuitButton){
+		Application.Quit();
+	}
+	if(isPlayButton){
+		Application.LoadLevel(0);
+	}
+	if(isInstructionButton){
+		Application.LoadLevel(2);
+	}
+}
+
