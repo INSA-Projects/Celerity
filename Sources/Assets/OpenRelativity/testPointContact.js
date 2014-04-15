a47b899010ce11ae1086596c510e9e88e24287f9
RmLtA <rm.liantsoa@gmail.com>
2014-04-15 13:24:59 +0200

    debut gestion munition

---
+++ Sources/Assets/OpenRelativity/testPointContact.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var tagArme = "Arme";
+
+function OnCollisionEnter(other : Collision) {
+		if (other.gameObject.tag == tagArme) {
+			KillMun();
+		/*faire l'affichage score*/
+		
+		/*augmentation du nombre de munition*/
+	}
+
+}
+
+function KillMun(){
+	Destroy(transform.gameObject);
+}
+
