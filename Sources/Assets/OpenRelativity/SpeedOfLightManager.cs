71816a7947474a2eadfebf844348934473c2449d
RmLtA <rm.liantsoa@gmail.com>
2014-04-24 00:16:40 +0200

    Mise en place des obstacles + deplacement des ennemis + dbt gesion STRManager

--- Sources/Assets/OpenRelativity/SpeedOfLightManager.cs
+++ Sources/Assets/OpenRelativity/SpeedOfLightManager.cs
@@ -5,19 +5,28 @@ public class SpeedOfLightManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		TRR.SPEEDOFLIGHT = 200f;
+		/*au depart les effets ne seront pas très visibles --> vitesse de la lumiere 800 par exemple*/
+		TRR.SPEEDOFLIGHT = 800f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// Allow changing the speed of light
-		if(Input.GetKeyDown("m"))
+		/*if(Input.GetKeyDown("m"))
 		{
 			TRR.SPEEDOFLIGHT -= 2f;
 		}
 		else if(Input.GetKeyDown("n"))
 		{
 			TRR.SPEEDOFLIGHT += 2f;
-		}
+		}*/
+
+		//Si le joueur a recuperé l'arme le speedoflight descend brusquement + explosion
+		//TRR.SPEEDOFLIGHT = 20f;
+
+		//Le joueur doit avancé jusqu'a la boule de sphere --> collision -->
+		//TRR.SPEEDOFLIGHT = 200f;
+
+
 	}
 }
