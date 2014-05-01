68136bf58912031584cd21f44c770d27888f75ba
RmLtA <rm.liantsoa@gmail.com>
2014-05-01 22:37:17 +0200

    modif cannon

---
+++ Sources/Assets/Scripts/Turrent.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+//var LookAtTarget : Transform;
+//var damp = 6.0;
+var bullitPrefab : Rigidbody;
+var savedTime = 0;
+var dureeDeVie=5;
+var speed = 1;
+
+function Start () {
+
+}
+
+function Update () {
+	//if(LookAtTarget){
+		//var rotate = Quaternion.LookRotation(LookAtTarget.position - transform.position);
+		//transform.rotation = Quaternion.Slerp(transform.rotation, rotate, Time.deltaTime*damp);
+		
+		
+		var seconds : int = Time.time;
+		var oddeven = (seconds % 2);
+		
+		if(oddeven){
+			Shoot(seconds);
+		}
+		
+		
+	//}
+}
+
+function Shoot(seconds){
+	if(seconds!=savedTime){
+	var bullit : Rigidbody = Instantiate(bullitPrefab, transform.position + new Vector3 (0.0f, 1.42f, -2.3f), Quaternion.identity);
+	//bullit.velocity = transform.TransformDirection(Vector3( 0, 0, speed ));
+	bullit.rigidbody.AddForce(transform.forward*1500);
+	
+	if (bullit) {
+		Destroy (bullit.gameObject, dureeDeVie);
+	}
+	
+	savedTime=seconds;
+	}
+}
