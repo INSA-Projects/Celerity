d91762f706544abddadb99b41a01c78602df245a
SuperValou <astry@hotmail.fr>
2014-05-24 14:09:03 +0200

    ajout items munition, vie, temps, et sons correspondants

---
+++ Sources/Assets/Scripts/Bonus_ammo.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bonus_ammo : MonoBehaviour {
+	bool firstEnter = true;
+	/*nombre de munition à rajouter lorsque vous ramassez des munitions*/
+	public int nbMunitionRecharge;
+	// mesh renderer to desactivate when you get the ammo
+	public GameObject bulletShape1;
+	public GameObject bulletShape2;
+	// GUI enabler
+	bool GUIEnabled = false;
+
+	void OnTriggerEnter(Collider col)	{
+		if (firstEnter && col.gameObject.tag == "Player") {
+			firstEnter = false;
+			this.gameObject.audio.Play();
+			LanceurBatteReondissanteCSharp.nbMaxMunition += nbMunitionRecharge;
+			GUIEnabled = true;
+			bulletShape1.renderer.enabled = false;
+			bulletShape2.renderer.enabled = false;
+			Invoke("destroy",5);
+			}
+	}
+
+	void destroy() {
+		this.gameObject.audio.Stop();
+		GUIEnabled = false;
+		Destroy (this.gameObject);
+	}
+
+	void OnGUI () {
+		if (GUIEnabled){
+			GUI.backgroundColor = Color.blue;
+			GUI.Box (new Rect ((Screen.width)/3,Screen.height - 200,(Screen.width)/3,60), "\nVous avez récupéré "+nbMunitionRecharge+" munitions.");
+		}
+	}
+}
---
+++ Sources/Assets/Scripts/Bonus_health.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bonus_health : MonoBehaviour {
+	bool GUIEnabled = false;
+	bool firstEnter = true;
+	public AudioClip audio;
+
+	void OnTriggerEnter(Collider other){
+		if (firstEnter && other.gameObject.tag == "Player") {
+			firstEnter = false;
+			GUIEnabled =true;
+			this.gameObject.renderer.enabled = false;
+			AudioSource.PlayClipAtPoint(audio, transform.position);
+			Health_Player.vie_courant+=1;
+			Invoke("destroy",4);
+		}
+	}
+
+	void destroy() {
+		Destroy(this.gameObject);
+	}
+
+	void OnGUI () {
+		if (GUIEnabled){
+			GUI.backgroundColor = Color.blue;
+			GUI.Box (new Rect ((Screen.width)/3,Screen.height - 200,(Screen.width)/3,60), "\nVous avez récupéré un point de vie.");
+		}
+	}
+	
+
+
+
+}
