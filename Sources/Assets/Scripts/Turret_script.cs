b75f6de0644093203ef133a49e6de5a249eec88e
RmLtA <rm.liantsoa@gmail.com>
2014-05-27 13:15:43 +0200

    tri turret ok

--- Sources/Assets/Scripts/Turret_script.cs
+++ Sources/Assets/Scripts/Turret_script.cs
@@ -7,23 +7,21 @@ public class Turret_script : MonoBehaviour {
 	public Transform bullitPrefab;
 	public int savedTime = 0;
 
-	public int dureeDeVie = 2;
-
-	public bool ok = false;
 
 	
 	// Update is called once per frame
 	void Update () {
 		Quaternion rotate;
 		int seconds;
-		int oddeven;
+		float oddeven;
+
 		if (LookAtTarget) {
 			rotate = Quaternion.LookRotation(LookAtTarget.position - transform.position)*Quaternion.Euler(0,180,0);
-			transform.rotation = Quaternion.Slerp (transform.rotation, rotate,(float) Time.time*damp);
+			transform.rotation = Quaternion.Slerp (transform.rotation, rotate,(float) Time.deltaTime*damp);
 			seconds = (int) Time.time;
-			oddeven = (seconds%10);
+			oddeven = (seconds%2);
 
-			if(oddeven == 0){
+			if(oddeven!=0){
 				Shoot(seconds);
 			}
 
@@ -34,18 +32,18 @@ public class Turret_script : MonoBehaviour {
 
 
 	void Shoot(int seconds){
-		if(seconds != savedTime && ok == true){
-			/*Transform bullit;
-			bullit = (Transform) Instantiate (bullitPrefab, transform.Find ("spawnPoint").transform.position, Quaternion.identity);
-			bullit.rigidbody.AddForce (transform.forward * 1000);*/
+		if(seconds != savedTime){
+		Transform bullit;
+		bullit = (Transform) Instantiate (bullitPrefab, transform.Find ("spawnPoint").transform.position, Quaternion.identity);
+		bullit.rigidbody.AddForce (transform.forward * 1000);
 
-			Rigidbody ball = (Rigidbody) Instantiate(bullitPrefab, transform.position, transform.rotation);
-			ball.rigidbody.AddForce(transform.forward*1500);
-			if (ball){
+			//Rigidbody ball = (Rigidbody) Instantiate(bullitPrefab, transform.position, transform.rotation);
+			//ball.rigidbody.AddForce(transform.forward*1000);
+			//if (ball){
 				//Destruction de l'instance de la balle
-				Destroy (ball.gameObject, dureeDeVie);
-				ok = true;
-			}
+			//	Destroy (ball.gameObject, dureeDeVie);
+			//	ok = true;
+			//}
 
 			savedTime = seconds;
 		}
---
+++ Sources/Assets/Scripts/Turret_script.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+var LookAtTarget : Transform;
+var damp = 6.0;
+var bullitPrefab : Transform;
+var savedTime = 0;
+
+
+function Update () {
+
+		if (LookAtTarget) {
+			var rotate = Quaternion.LookRotation(LookAtTarget.position - transform.position)*Quaternion.Euler(0,180,0);
+			transform.rotation = Quaternion.Slerp (transform.rotation, rotate,Time.deltaTime*damp);
+			var seconds : int = Time.time;
+			var oddeven = (seconds%2);
+
+			if(oddeven){
+				Shoot(seconds);
+			}
+		}
+}
+
+function Shoot(seconds){
+	if(seconds != savedTime){
+		var bullit = Instantiate (bullitPrefab, transform.Find ("spawnPoint").transform.position, Quaternion.identity);
+		bullit.rigidbody.AddForce (transform.forward * 1000);
+		
+		savedTime = seconds;
+	}
+}
